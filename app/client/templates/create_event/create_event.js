0a2b4a7fc596531063707be0d9b8828acf9ce85f
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-29 10:21:01 -0800

    made the events insert

--- app/client/templates/create_event/create_event.js
+++ app/client/templates/create_event/create_event.js
@@ -2,6 +2,28 @@
 /* CreateEvent: Event Handlers */
 /*****************************************************************************/
 Template.CreateEvent.events({
+	 /*
+	 events:
+created_by:
+name:
+location:
+date:
+picture_URL:
+cost:*/
+'submit form': function(e, tmpl){
+	e.preventDefault();
+
+	Events.insert({
+		name: tmpl.find('#name').value,
+		create_by: Meteor.userId(),
+		location: tmpl.find('#location').value,
+		date: tmpl.find('#eventDate').value,
+		picture_url: tmpl.find('#picture').value,
+		cost: tmpl.find('#cost').value,
+	});
+	console.log("event added");
+
+}
 });
 
 /*****************************************************************************/
