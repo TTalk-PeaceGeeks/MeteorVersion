0528b46f047acf710cdaefba4af2501a0f9409fa
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-28 18:04:28 -0800

    added profiles

--- app/lib/collections/profiles.js
+++ app/lib/collections/profiles.js
@@ -4,20 +4,6 @@ Profiles = new Mongo.Collection('profiles');
 if (Meteor.isServer) {
   Profiles.allow({
     insert: function (userId, doc) {
-      return false;
-    },
-
-    update: function (userId, doc, fieldNames, modifier) {
-      return false;
-    },
-
-    remove: function (userId, doc) {
-      return false;
-    }
-  });
-
-  Profiles.deny({
-    insert: function (userId, doc) {
       return true;
     },
 
@@ -29,4 +15,5 @@ if (Meteor.isServer) {
       return true;
     }
   });
+
 }
